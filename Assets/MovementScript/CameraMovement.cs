afd43ad86f513f4a2aa3c66bc7e4ce3d08dfbf1d
dapurpkilla <jlk5717@louisiana.edu>
2017-01-24 15:20:50 -0600

    MovementBasics

---
+++ Assets/MovementScript/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Use this for initialization
+    public float H = 2.0F;
+    
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = H * Input.GetAxis("Mouse X");
+        transform.Rotate(0, h, 0); 
+	}
+}
---
+++ Assets/MovementScript/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        CharacterController control = GetComponent<CharacterController>();
+        if (control.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        control.Move(moveDirection * Time.deltaTime);
+        if (transform.position.y < -10)
+        {
+            transform.position = new Vector3(0, 0, 0); 
+        }
+    }
+
+}
