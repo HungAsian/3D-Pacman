ac9fe6f7b0045a68ba122f884ce29572b2473e44
dapurpkilla <jlk5717@louisiana.edu>
2017-01-26 15:19:23 -0600

    Movement improved
    kl;jkl;

--- Assets/MovementScript/CameraMovement.cs
+++ Assets/MovementScript/CameraMovement.cs
@@ -6,14 +6,18 @@ public class CameraMovement : MonoBehaviour {
 
 	// Use this for initialization
     public float H = 2.0F;
-    
+    public Transform player;
+    public Vector3 movement; 
 	void Start () {
-		
+        movement = new Vector3(player.position.x, player.position.y + 8.0f, player.position.z - 8.0f); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float h = H * Input.GetAxis("Mouse X");
-        transform.Rotate(0, h, 0); 
+        movement = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * H, Vector3.up) * movement;
+        transform.position = player.position + movement;
+        transform.LookAt(player.position);
+        
 	}
 }
